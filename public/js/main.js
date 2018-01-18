73153eb348d851e2d21320c3b034837dd53188a1
hayk <hayk.atoyan111@gmail.com>
2018-01-18 03:00:16 -0800

    initial state

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function($){
+$('.statistic-counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+});
+/*$('.statistic-counter').counterUp({
+    delay: 10,
+    time: 2000
+});*/
+
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express'),
+      logger = require('morgan'),
+      bodyParser = require('body-parser'),
+      path = require('path'),
+      app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//app.engine('html', require('ejs').renderFile);
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function (req,res) {
+    res.render('index');
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.send('error');
+});
+
+app.listen('2000',function () {
+    console.log('server runs on port 2000');
+});
